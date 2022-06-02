98566c5bae8b1dc72d802ac475a8c952acb5d1be
Ivan <ivan241578@gmail.com>
2022-06-02 19:32:22 +0300

    Task41

---
+++ Task41/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите числа через пробел: ");
+int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0) count++;
+}
+Console.WriteLine(count);
